0ef18f62f958786cdb970800df45cdfb5813f12b
Maxime Lacroix <maximeglacroix@gmail.com>
2021-10-09 08:15:02 -0400

    small change to main to test for tomorrow's game

--- R/main.R
+++ R/main.R
@@ -1,4 +1,8 @@
 source("R/weekly_estimator.R")
 
-week_predictor(week_to_predict = 5,
-               year_to_predict = 2021) 
+estimates_week_5 <- week_predictor(week_to_predict = 5,
+                                   year_to_predict = 2021) 
+
+
+estimates_week_5 %>% 
+  dplyr::filter(defteam_week == "NYJ" | posteam == "NYJ")
